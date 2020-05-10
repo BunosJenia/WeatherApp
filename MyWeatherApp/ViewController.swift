c483212f25b3bb32ffb96c901936bf6592ca35cb
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-05-10 21:52:12 +0300

    Initial Commit

---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by Yauheni Bunas on 5/10/20.
+//  Copyright © 2020 Yauheni Bunas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
